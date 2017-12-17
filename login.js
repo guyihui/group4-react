244e9af29548204ddc44572641c8351f8b81f711
DreamlandSky <1739014279@qq.com>
2017-12-17 12:25:37 +0800

    basic function of signup and login

--- login.js
+++ login.js
@@ -1,24 +1,78 @@
-
-function register(Username, Password){
-
+function Map() {
+    var struct = function (key, value) {
+        this.key = key;
+        this.value = value;
+    }
+    var put = function (key, value) {
+        for (var i = 0; i < this.arr.length; i++) {
+            if (this.arr[i].key === key) {
+                this.arr[i].value = value;
+                return;
+            }
+        }
+        this.arr[this.arr.length] = new struct(key, value);
+    }
+    var get = function (key) {
+        for (var i = 0; i < this.arr.length; i++) {
+            if (this.arr[i].key === key) {
+                return this.arr[i].value;
+            }
+        }
+        return null;
+    }
+    var remove = function (key) {
+        var v;
+        for (var i = 0; i < this.arr.length; i++) {
+            v = this.arr.pop();
+            if (v.key === key) {
+                continue;
+            }
+            this.arr.unshift(v);
+        }
+    }
+    var isEmpty = function () {
+        return this.arr.length <= 0;
+    }
+    this.arr = new Array();
+    this.get = get;
+    this.put = put;
+    this.remove = remove;
+    this.isEmpty = isEmpty;
 }
 
-function reg_success(){
+var map = new Map();
 
+function Have_Username(Username){
+    if(map.isEmpty()){
+        return "-1";
+    }
+    var res = map.get(Username);
+    if(res == null){
+        return "-1";
+    }
+    return res;
 }
-
-function reg_failed(){
-
+function signup(Username, Password){
+    if(Have_Username(Username) != "-1"){
+        return "Username is already existed.";
+    }
+    else{
+        map.put(Username,Password);
+        return "Success!";
+    }
 }
 
 function login(Username, Password){
-
-}
-
-function login_success(){
-
-}
-
-function login_failed(){
-
+    var password = Have_Username(Username);
+    if(password == "-1"){
+        return "Username doesn't exist!";
+    }
+    else{
+        if(password != Password){
+            return "Wrong password!";
+        }
+        else{
+            return "Login success!";
+        }
+    }
 }
